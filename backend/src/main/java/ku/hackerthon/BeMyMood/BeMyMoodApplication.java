4841927a23a638728a950f02d74cd5658868135d
KONKUK\s_rudwhd515 <rudwhd515@gmail.com>
2023-08-06 23:56:18 +0900

    setup : initialize back-end project

---
+++ backend/src/main/java/ku/hackerthon/BeMyMood/BeMyMoodApplication.java
@@ -0,0 +1,13 @@
+package ku.hackerthon.BeMyMood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeMyMoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeMyMoodApplication.class, args);
+	}
+
+}
