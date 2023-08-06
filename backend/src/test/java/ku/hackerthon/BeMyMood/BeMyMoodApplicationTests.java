533c08ef4ee932c7927bde9f0db793cb40e08b95
KONKUK\s_rudwhd515 <rudwhd515@gmail.com>
2023-08-06 23:18:20 +0900

    setup : backend

---
+++ backend/src/test/java/ku/hackerthon/BeMyMood/BeMyMoodApplicationTests.java
@@ -0,0 +1,13 @@
+package ku.hackerthon.BeMyMood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeMyMoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
