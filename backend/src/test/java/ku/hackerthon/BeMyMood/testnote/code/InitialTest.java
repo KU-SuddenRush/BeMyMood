1df7cbbdf46d4066b2fc011c0235668c6a5be84c
KONKUK\s_rudwhd515 <rudwhd515@gmail.com>
2023-08-07 01:29:20 +0900

    test : 테스트시 h2 데이터 베이스 사용

---
+++ backend/src/test/java/ku/hackerthon/BeMyMood/testnote/code/InitialTest.java
@@ -0,0 +1,15 @@
+package ku.hackerthon.BeMyMood.testnote.code;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class InitialTest {
+
+    @Test
+    void initial_test() {
+        int a = 1;
+
+        assertThat(a).isEqualTo(1);
+    }
+}
