38d990d6d53dba4e90753c914f01297d301660a3
ro-el <alcxzp@gmail.com>
2023-08-07 20:31:41 +0900

    feat: spot 관련 엔티티 생성

---
+++ backend/src/main/java/ku/hackerthon/BeMyMood/domain/spot/tag/SpotLocation.java
@@ -0,0 +1,24 @@
+package ku.hackerthon.BeMyMood.domain.spot.tag;
+
+import ku.hackerthon.BeMyMood.domain.spot.Spot;
+import ku.hackerthon.BeMyMood.domain.tag.Location;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SpotLocation {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "spot_location_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "spot_id")
+    private Spot spot;
+
+    @ManyToOne
+    @JoinColumn(name = "location_id")
+    private Location location;
+}
