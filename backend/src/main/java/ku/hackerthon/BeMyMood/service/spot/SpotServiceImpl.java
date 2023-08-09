43c28037feed3194b6cfc3b8bad29d796eb7d4ac
KONKUK\s_rudwhd515 <rudwhd515@gmail.com>
2023-08-09 16:37:15 +0900

    chore : 컴파일 에러 해결

--- backend/src/main/java/ku/hackerthon/BeMyMood/service/spot/SpotServiceImpl.java
+++ backend/src/main/java/ku/hackerthon/BeMyMood/service/spot/SpotServiceImpl.java
@@ -1,10 +1,13 @@
 package ku.hackerthon.BeMyMood.service.spot;
 
+import ku.hackerthon.BeMyMood.domain.mood.Mood;
 import ku.hackerthon.BeMyMood.domain.spot.Spot;
 import ku.hackerthon.BeMyMood.respository.SpotRepository;
 import ku.hackerthon.BeMyMood.service.spot.dto.SpotParams;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -17,5 +20,10 @@ public class SpotServiceImpl implements SpotService {
         Spot spot = Spot.ofParams(spotParams);
         spotRepository.save(spot);
         return spot;
+    }
+
+    @Override
+    public List<Spot> searchAllByMoods(List<Mood> moods) {
+        return null;
     }
 }
