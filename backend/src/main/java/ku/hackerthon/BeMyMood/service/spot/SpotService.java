0efd6fa6ea873a7ac52346bfcd508fc4421c4e8f
KONKUK\s_rudwhd515 <rudwhd515@gmail.com>
2023-08-09 16:36:28 +0900

    feat : SpotService, 무드 리스트를 통해 Spot 리스트를 찾는 기능

--- backend/src/main/java/ku/hackerthon/BeMyMood/service/spot/SpotService.java
+++ backend/src/main/java/ku/hackerthon/BeMyMood/service/spot/SpotService.java
@@ -1,9 +1,14 @@
 package ku.hackerthon.BeMyMood.service.spot;
 
+import ku.hackerthon.BeMyMood.domain.mood.Mood;
 import ku.hackerthon.BeMyMood.domain.spot.Spot;
 import ku.hackerthon.BeMyMood.service.spot.dto.SpotParams;
+
+import java.util.List;
 
 public interface SpotService {
 
     Spot register(SpotParams spotParams);
+
+    List<Spot> searchAllByMoods(List<Mood> moods);
 }
