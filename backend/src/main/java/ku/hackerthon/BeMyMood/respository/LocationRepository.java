3a540fb080621ec5c22b9f2f7ffc63bc7b5e511f
KONKUK\s_rudwhd515 <rudwhd515@gmail.com>
2023-08-09 15:59:47 +0900

    feat : LocationRepository 검색어를 포함하는 모든 Location검색하는 기능 구현

---
+++ backend/src/main/java/ku/hackerthon/BeMyMood/respository/LocationRepository.java
@@ -0,0 +1,21 @@
+package ku.hackerthon.BeMyMood.respository;
+
+import ku.hackerthon.BeMyMood.domain.location.Location;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Repository
+public class LocationRepository {
+
+    private final EntityManager em;
+
+    public List<Location> findContainsByName(String name) {
+        return em.createQuery("select l from Location l where l.name like %:name%", Location.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
