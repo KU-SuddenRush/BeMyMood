f8b3f749edc62988607e4e67b94000599dd5b263
ro-el <alcxzp@gmail.com>
2023-08-12 21:55:42 +0900

    test: 테스트 코드 수정

--- backend/src/test/java/ku/hackerthon/BeMyMood/service/member/MemberServiceImplTest.java
+++ backend/src/test/java/ku/hackerthon/BeMyMood/service/member/MemberServiceImplTest.java
@@ -4,6 +4,7 @@ import ku.hackerthon.BeMyMood.domain.member.Member;
 import ku.hackerthon.BeMyMood.domain.member.mood.PreferredMoods;
 import ku.hackerthon.BeMyMood.domain.mood.Mood;
 import ku.hackerthon.BeMyMood.domain.mood.MoodType;
+import ku.hackerthon.BeMyMood.dto.member.MemberJoinParams;
 import ku.hackerthon.BeMyMood.service.mood.MoodService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -28,7 +29,7 @@ class MemberServiceImplTest {
     @DisplayName("좋아하는 무드 입력 테스트")
     void setMemberService() throws Exception {
         // given
-        List<String> colors = List.of("#뮤트한", "#비비드한", "#   차분한", "#빛바랜", "#무채색의", "#파스텔의", "#블랙앤화이트",
+        List<String> colors = List.of("#뮤트한", "#비비드한", "#차분한", "#빛바랜", "#무채색의", "#파스텔의", "#블랙앤화이트",
                 "#자연적인", "#투명한", "#매트한", "#형광의", "#쿨한", "#웜한");
         for (String color : colors) {
             Mood tempMood = new Mood(color, MoodType.COLOR);
@@ -36,12 +37,14 @@ class MemberServiceImplTest {
         }
                 
         // when
-        List<String> selectMoods = List.of("#무채색의", "#파스텔의", "#블랙앤화이트"); // 43, 44, 45
+        List<String> selectMoods = List.of("#무채색의", "#파스텔의", "#블랙앤화이트");
         List<Mood> moods = new ArrayList<>();
         for (String selectMood : selectMoods) {
             moods.add(moodService.getByName(selectMood));
         }
 
+        MemberJoinParams newMember = new MemberJoinParams("testUser", "test@gmail.com", "12345");
+        memberService.join(newMember);
         Long memberId = 1L;
         Member member = memberService.searchById(memberId);
 
