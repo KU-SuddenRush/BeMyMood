659a57c3f01ce44652cb92fbbf75c10c3f46e27b
YoonSub Lim <yslim37@naver.com>
2023-08-12 17:53:39 +0900

    feat: filter & collection view section 구분

--- frontend/BeMyMood/SpotListModal/FirstViewController.swift
+++ frontend/BeMyMood/SpotListModal/FirstViewController.swift
@@ -6,13 +6,52 @@
 //
 
 import UIKit
+import SwiftUI
 
 class FirstViewController: UIViewController {
 
+    //MARK: - UIComponents
+    
+    let filterSection = UIView().then{
+        $0.backgroundColor = .lightGray
+    }
+    
+    let spotCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout()).then{
+        $0.backgroundColor = .blue
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .green
+        hierarchy()
+        layout()
     }
 
+}
+
+extension FirstViewController {
+    
+    func hierarchy(){
+        self.view.addSubview(filterSection)
+        self.view.addSubview(spotCollectionView)
+    }
+    
+    func layout(){
+        filterSection.snp.makeConstraints{ make in
+            make.top.leading.trailing.equalToSuperview()
+            make.height.equalTo(50).priority(.high)
+            make.bottom.equalTo(spotCollectionView.snp.top)
+        }
+        
+        spotCollectionView.snp.makeConstraints{ make in
+            make.bottom.leading.trailing.equalToSuperview()
+        }
+    }
+}
+
+
+struct FirstVCPreView:PreviewProvider {
+    static var previews: some View {
+        FirstViewController().toPreview()
+    }
 }
