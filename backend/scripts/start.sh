e98c141fe18afbc0b16b2d9300a36881a07b51d9
60jong <rudwhd515@gmail.com>
2023-08-13 15:54:37 +0900

    fix : 프로젝트 패키지 구조 수정

--- backend/scripts/start.sh
+++ backend/scripts/start.sh
@@ -1,4 +1,4 @@
-PROJECT_ROOT="/home/ubuntu/BeMyMood/artifact"
+PROJECT_ROOT="/home/ubuntu/BeMyMood/artifact/backend"
 JAR_FILE="$PROJECT_ROOT/build/libs/*.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
