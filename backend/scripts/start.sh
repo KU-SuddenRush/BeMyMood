32af3be0584a2ba03b02ad4279bda4fa27e79e42
60jong <rudwhd515@gmail.com>
2023-08-13 16:09:08 +0900

    fix : 배포 시 active profile 수정

--- backend/scripts/start.sh
+++ backend/scripts/start.sh
@@ -7,9 +7,10 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 TIME_NOW=$(date +%c)
 
+ACTICE_PROFILE="release"
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -jar $JAR_FILE --spring.profiles.active=develop > $APP_LOG 2> $ERROR_LOG &
+nohup java -jar $JAR_FILE --spring.profiles.active=$ACTICE_PROFILE > $APP_LOG 2> $ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
