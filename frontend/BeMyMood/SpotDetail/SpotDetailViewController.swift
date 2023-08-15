b157f1211ce5521dcebba2e9d19ce9c30122527e
YoonSub Lim <yslim37@naver.com>
2023-08-16 05:57:38 +0900

    feat: SpotDetail 내 Tag Collection View 구성

--- frontend/BeMyMood/SpotDetail/SpotDetailViewController.swift
+++ frontend/BeMyMood/SpotDetail/SpotDetailViewController.swift
@@ -12,16 +12,23 @@ import SwiftUI
 
 class SpotDetailViewController: UIViewController {
     
+    var tempData = SpotDetailInfos(spotId: 3, bookmarked: true, spotName: "누데이크 성수점", spotThumbnailImageUrl: ["https://picsum.photos/200/300","https://picsum.photos/200/300","https://picsum.photos/200/300"], categoryName: "카페", moodNames: ["힙한","소품이많은","무채색","어두운","특이한소재","포인트컬러","정적인","키치한","세련된","단조로운"], address: "서울 성동구 성수이로7길 26 1층", url: "http://www.nudake.com", description: "대법관의 임기는 6년으로 하며, 법률이 정하는 바에 의하여 연임할 수 있다. 대통령이 궐위된 때 또는 대통령 당선자가 사망하거나 판결 기타의 사유로 그 자격을 상실한 때에는 60일 이내에 후임자를 선거한다. 국정감사 및 조사에 관한 절차 기타 필요한 사항은 법률로 정한다. 국방상 또는 국민경제상 긴절한 필요로 인하여 법률이 정하는 경우를 제외하고는, 사영기업을 국유 또는 공유로 이전하거나 그 경영을 통제 또는 관리할 수 없다.", recommends: ["음료", "전시", "디저트"], menuImageUrls: ["https://picsum.photos/200/300","https://picsum.photos/200/300","https://picsum.photos/200/300", "https://picsum.photos/200/300"])
+    
     //MARK: - UIComponents
     let categoryLabel = UILabel().then{
-        $0.backgroundColor = .lightGray
+        $0.backgroundColor = .white
         $0.text = "Category Label"
         $0.textColor = .orange
     }
     
-    let tagCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout()).then{
-        $0.backgroundColor = .blue
-    }
+    let tagCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let tagCV = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        tagCV.backgroundColor = .white
+        tagCV.showsHorizontalScrollIndicator = false
+        return tagCV
+    }()
     
     let slidingTabVC = UISlidingTabController().then{
         $0.addItem(item: DetailViewController(), title: "상세정보")
@@ -39,10 +46,20 @@ class SpotDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = .systemTeal
+        self.view.backgroundColor = .white
         
         hierarchy()
         layout()
+        
+        dataInit()
+        
+        tagCollectionView.dataSource = self
+        tagCollectionView.delegate = self
+        tagCollectionView.register(SpotTagCell.self, forCellWithReuseIdentifier: "spotTagCell")
+    }
+    
+    func dataInit(){
+        categoryLabel.text = tempData.categoryName
     }
     
 }
@@ -84,8 +101,58 @@ extension SpotDetailViewController{
     }
 }
 
+//MARK: - Spot Tag Collection View
+extension SpotDetailViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return tempData.moodNames.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+                
+        let spotTagCell = tagCollectionView.dequeueReusableCell(withReuseIdentifier: "spotTagCell", for: indexPath) as! SpotTagCell
+        
+        spotTagCell.tagLabel.text = "#" + tempData.moodNames[indexPath.row]
+        
+        return spotTagCell
+    }
+    
+    func calculateTagCellWidth(for tagText: String) -> CGFloat {
+        // 셀 폭을 계산하는 로직을 작성합니다. 텍스트의 길이에 따라 동적으로 폭을 설정하거나, 고정된 값으로 설정할 수 있습니다.
+        let textWidth = tagText.size(withAttributes: [.font: UIFont.systemFont(ofSize: 14.0)]).width
+        let cellWidth = textWidth + 35.0 // 예시로 폭에 텍스트 너비를 추가하고 여백을 더합니다.
+        
+        return cellWidth
+    }
+    
+    // CollectionView Cell의 Size
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let cellWidth = calculateTagCellWidth(for: tempData.moodNames[indexPath.row])
+        
+        return CGSize(width: cellWidth, height: collectionView.frame.height / 2 - 5)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 10 // 가로 간격 설정
+    }
+}
+
 struct SpotDetailVCPreView:PreviewProvider {
     static var previews: some View {
         SpotDetailViewController().toPreview()
     }
+}
+
+struct SpotDetailInfos{
+    let spotId: Int
+    let bookmarked: Bool
+    let spotName: String
+    let spotThumbnailImageUrl: [String]
+    let categoryName: String
+    let moodNames: [String]
+    let address: String
+    let url: String
+    let description: String
+    let recommends: [String]
+    let menuImageUrls: [String]
 }
