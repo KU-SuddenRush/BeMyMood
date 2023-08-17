b7ed32b56730b3f73af90cb9ad94ca8c83127a4c
60jong <rudwhd515@gmail.com>
2023-08-17 09:19:32 +0900

    git : git 충돌 해결

---
+++ backend/src/main/java/ku/hackerthon/BeMyMood/dto/web/request/SigninRequestDto.java
@@ -0,0 +1,13 @@
+package ku.hackerthon.BeMyMood.dto.web.request;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class SigninRequestDto {
+
+    private String email;
+    private String password;
+}
